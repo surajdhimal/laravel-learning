dbd7ab6a319df85fb9ca9d686eb7e0006eebf6e6
Anurag-regmi <www.surajdhimal1234@gmail.com>
2025-01-14 17:21:50 +0545

    Update StudentController to handle contact-related functionality

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\Student;
+use App\Models\Contact;
 use Illuminate\Http\Request;
 
 class StudentController extends Controller
@@ -13,8 +14,25 @@ class StudentController extends Controller
      */
     public function index()
     {
-        $users = Student::all();
-        return $users;
+        // $students = Student::all();
+        $students = Student::with('contacts')->get();
+        // $students = Student::with('contacts')->find(2);
+        // $students = Student::with('contacts')->where('city','Kathmandu')->get();
+        // $students = Student::withWhereHas('contacts', function($query){
+        //     $query->where('city', 'Province 3');
+        // })->get();
+        // $students = Student::where('age', 21)->withWherehas('contacts', function($query){
+        //     $query->where('city', 'Province 3');
+        // })->get();
+        // $students = Student::where('age', 21)->Wherehas('contacts', function($query){
+        //     $query->where('city', 'Province 3');
+        // })->get();
+
+        return $students;
+        // echo $students->name . "<br>";
+        // echo $students->contacts->email;
+
+
     }
 
     /**
@@ -22,16 +40,28 @@ class StudentController extends Controller
      */
     public function create()
     {
-        $students = new Student;
+        // $students = new Student;
 
         // $students->name = "Prakash Koirala";
         // $students->age = 25;
         // $students->city = "Biratnagar";
 
-        $students->name = "Aisha Thapa";
-        $students->age = 24;
+        // $students->name = "Aisha Thapa";
+        // $students->age = 24;
 
-        $students->save();
+        // $students->save();
+
+        $student = Student::create([
+            'name' => 'Ravi Sharma',
+            'age' => 25,
+            'city' => 'Kathmandu'
+        ]);
+        $student->contacts()->create([
+            'address' => '123 Main Street, Kathmandu',
+            'city' => 'Kathmandu',
+            'email' => 'ravi.sharma@email.com',
+            'phone' => '+977 1 2345678'
+        ]);
     }
 
     /**
