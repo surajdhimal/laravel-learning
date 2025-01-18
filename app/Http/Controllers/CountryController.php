f4a903c850dccf6f4190a4d3c082f080e74c3476
Anurag-regmi <www.surajdhimal1234@gmail.com>
2025-01-18 14:02:35 +0545

    Implemented CountryController to handle fetching blogs through related clients (Has Many Through)

---
+++ app/Http/Controllers/CountryController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Country;
+use Illuminate\Http\Request;
+
+class CountryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        // $country = Country::find(2);
+        // return $country->blogs;
+
+        // $country = Country::with('blogs')->find(2);
+        // $country = Country::with('blogs')->get();
+        $country = Country::with('clients')->with('blogs')->find(2);
+        return $country;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
