43656bb49c204aa7314a113a06d9fa8600bce71d
Anurag-regmi <www.surajdhimal1234@gmail.com>
2025-01-20 21:03:45 +0545

    Refactored Video model to add new functionality

--- app/Models/Video.php
+++ app/Models/Video.php
@@ -9,4 +9,19 @@ class Video extends Model
     public function contributors(){
         return $this->morphMany(Contributor::class, 'contributor');
     }
+
+    public function latestComment(){
+        return $this->morphOne(Contributor::class, 'contributor')->latestOfMany();
+    }
+
+    public function oldestComment(){
+        return $this->morphOne(Contributor::class, 'contributor')->oldestOfMany();
+    }
+
+    public function BestComment(){
+        return $this->morphOne(Contributor::class, 'contributor')->ofMany('likes', 'max');
+    }
+    public function leastComment(){
+        return $this->morphOne(Contributor::class, 'contributor')->ofMany('likes', 'min');
+    }
 }
