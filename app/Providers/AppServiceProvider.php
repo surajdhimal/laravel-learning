4d66307e074abbf9243046a7dc85cedaac154e0b
Anurag-regmi <www.surajdhimal1234@gmail.com>
2025-01-23 16:56:02 +0545

    Registered observers in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,11 @@
 
 namespace App\Providers;
 
+use App\Http\Controllers\ThreadController;
+use App\Models\Account;
+use App\Models\Thread;
+use App\Observers\AccountObserver;
+use App\Observers\ThreadObserver;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Account::observe(AccountObserver::class);
+        Thread::observe(ThreadObserver::class);
     }
 }
