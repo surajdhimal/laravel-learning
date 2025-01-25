4d185f087bb769ca9732f016d883d405368b7ee2
Anurag-regmi <www.surajdhimal1234@gmail.com>
2025-01-25 06:58:31 +0545

    Added query scope in Member model to filter by membership status

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -4,11 +4,36 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Post;
+use App\Models\Scopes\MemberScope;
+use Illuminate\Contracts\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Attributes\ScopedBy;
 
+#[ScopedBy([MemberScope::class])]
 class Member extends Model
 {
-    protected $guarded = [];
+    // protected static function booted(): void
+    // {
+    //     // static::addGlobalScope("userdetail", function(Builder $builder){
+    //     //     $builder->where("status", 1);
+    //     // });
+
+    //     static::addGlobalScope(new MemberScope);
+    // }
+
     public function posts(){
-        return $this->hasMany(Post::class, 'user_id');
+        return $this->hasmany(Post::class, "user_id");
+    }
+    
+    public function scopeActive($query){
+        return $query->where("status", 1);
+    }
+
+    public function scopeCity($query, $cityName){
+        return $query->where("city", $cityName);
+        // return $query->whereIn("city", $cityName);
+    }
+
+    public function scopeSort($query){
+        return $query->orderBy("name", "asc");
     }
 }
