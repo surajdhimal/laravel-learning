3354ba2e560b66450b36dffa660433f59a45b48f
Anurag-regmi <www.surajdhimal1234@gmail.com>
2025-01-25 06:59:05 +0545

    Added query scope files for filtering data in models

---
+++ app/Models/Scopes/MemberScope.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models\Scopes;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Scope;
+
+class MemberScope implements Scope
+{
+    /**
+     * Apply the scope to a given Eloquent query builder.
+     */
+    public function apply(Builder $builder, Model $model): void
+    {
+        $builder->select("id","name","email")->with("posts:title,description,user_id")->where("status",1);
+    }
+}
